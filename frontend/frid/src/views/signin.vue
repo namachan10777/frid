e883792aef1ff3ef9543ed98b008a5baa836bfae
ioComk <rw110407@gmail.com>
2019-09-10 13:02:28 +0900

    Update: Signinページのスタイル整形

--- frontend/frid/src/views/signin.vue
+++ frontend/frid/src/views/signin.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="signin">
     <section class="logo">
-      <img src="../assets/logo.png">
+      <img src="../assets/logo.png" />
     </section>
     <section class="login">
-    <button type="button" @click="signIn">ログイン</button>
+      <button class="login-button" @click="signIn">ログイン</button>
     </section>
   </div>
 </template>
@@ -22,15 +22,15 @@ export default {
   methods: {
     // ログイン
     signIn() {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider);
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider);
     }
   },
 
   mounted: function() {
     firebase.auth().onAuthStateChanged(user => {
-      if(user){
-        this.$router.push('/home')
+      if (user) {
+        this.$router.push("/home");
       }
     });
   }
@@ -38,13 +38,29 @@ export default {
 </script>
 
 <style>
-.logo{
-  display: inline-block;
-  width: 300px;
+.logo {
+  max-width: 300px;
   margin: 0 auto;
 }
 
-.login{
-  margin: 0 auto; 
+.login {
+  padding-top: 40px;
+  max-width: 140px;
+  margin: 0 auto;
+}
+
+.login-button {
+  display: inline-block;
+  padding: 0.4em 3em;
+  text-decoration: none;
+  background: #009e8c; /*ボタン色*/
+  color: #fff;
+  border-radius: 8px;
+}
+.login-button:active {
+  /*ボタンを押したとき*/
+  -webkit-transform: translateY(2px);
+  transform: translateY(2px); /*下に動く*/
+  border-bottom: none; /*線を消す*/
 }
 </style>
