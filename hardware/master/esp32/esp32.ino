f7800872362a711cd95a277633995e0e999fc207
Nakano Masaki <namachan10777@gmail.com>
2019-09-17 16:36:31 +0900

    ダメわね

---
+++ hardware/master/esp32/esp32.ino
@@ -0,0 +1,42 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* SSID = "TestAP";
+const char* PASSWORD = "weak_pass";
+const char* ENDPOINT = "https://us-central1-frid-vue.cloudfunctions.net/updateTemp";
+
+void setup() {
+	Serial.begin(115200);
+	while (!Serial);
+
+	Serial.println("Connecting to WiFi");
+	while (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(SSID, PASSWORD);
+		Serial.print(".");
+		delay(500);
+	}
+
+	Serial.println(" done!");
+}
+
+String buildBody(int id, int temp) {
+  auto json = String("{\"id\":\"");
+  json.concat(String(id));
+  json.concat(String("\", \"temperature\":"));
+  json.concat(String(temp));
+  json.concat(String("}"));
+  return json;
+}
+
+void loop() {
+	auto temp = random(0, 100);
+	HTTPClient http;
+  http.begin(ENDPOINT);
+	http.addHeader("Content-Type", "application/json");
+  auto body = buildBody(0, temp);
+	auto httpResponseCode = http.POST(body);
+	http.end();
+  Serial.print("http status code");
+	Serial.println(httpResponseCode);
+	delay(1000);
+}
