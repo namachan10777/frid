8eef10cc1a84d8975e940ab2a0c64b2ce13cf717
namachan10777 <namachan10777@gmail.com>
2019-09-19 02:19:02 +0900

    fix indent

--- hardware/master/master.ino
+++ hardware/master/master.ino
@@ -2,21 +2,21 @@
 #include <HTTPClient.h>
 
 const char* SSID = "TestAP";
-const char* PASSWORD = "weak_pass";
+const char* PASSWORD = "";
 const char* ENDPOINT = "https://us-central1-frid-vue.cloudfunctions.net/updateTemp";
 
 void setup() {
-	Serial.begin(115200);
-	while (!Serial);
+  Serial.begin(115200);
+  while (!Serial);
 
-	Serial.println("Connecting to WiFi");
-	while (WiFi.status() != WL_CONNECTED) {
-    WiFi.begin(SSID, PASSWORD);
-		Serial.print(".");
-		delay(500);
-	}
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+  WiFi.begin(SSID, PASSWORD);
+  	Serial.print(".");
+	delay(500);
+  }
 
-	Serial.println(" done!");
+  Serial.println(" done!");
 }
 
 String buildBody(int id, int temp) {
@@ -29,14 +29,14 @@ String buildBody(int id, int temp) {
 }
 
 void loop() {
-	auto temp = random(0, 100);
-	HTTPClient http;
+  auto temp = random(0, 100);
+  HTTPClient http;
   http.begin(ENDPOINT);
-	http.addHeader("Content-Type", "application/json");
+  http.addHeader("Content-Type", "application/json");
   auto body = buildBody(0, temp);
-	auto httpResponseCode = http.POST(body);
-	http.end();
+  auto httpResponseCode = http.POST(body);
+  http.end();
   Serial.print("http status code");
-	Serial.println(httpResponseCode);
-	delay(1000);
+  Serial.println(httpResponseCode);
+  delay(1000);
 }
